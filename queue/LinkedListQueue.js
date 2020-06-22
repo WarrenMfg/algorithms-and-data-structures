3578ce674cc06a067458ea8958ac78a3ce05f75a
WarrenMfg <warrenmfg@icloud.com>
2020-06-22 14:44:13 -0400

    ES6, pseudoclassical, and linked list queues

---
+++ queue/LinkedListQueue.js
@@ -0,0 +1,50 @@
+class LinkedListQueue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  createNewNode(value) {
+    return {
+      value,
+      next: null
+    };
+  }
+
+  // similar to .push() for linked list
+  enqueue(value) {
+    const newNode = this.createNewNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    return ++this.length;
+  }
+
+  // similar to .shift() for linked list
+  dequeue() {
+    if (!this.head) return null;
+
+    const dequeued = this.head;
+
+    if (this.head === this.tail) {
+      this.tail = null;
+    }
+
+    this.head = dequeued.next;
+    this.length--;
+
+    return dequeued.value;
+  }
+
+  size() {
+    return this.length;
+  }
+
+}
---
+++ queue/PseudoclassicalQueue.js
@@ -0,0 +1,27 @@
+var PseudoclassicalQueue = function() {
+  this.storage = {};
+  this.head = 0;
+  this.tail = 0;
+};
+
+PseudoclassicalQueue.prototype.enqueue = function(value) {
+  this.storage[this.tail] = value;
+  this.tail++;
+};
+
+PseudoclassicalQueue.prototype.dequeue = function() {
+  if (this.tail > this.head) {
+    let value = this.storage[this.head];
+    delete this.storage[this.head];
+    this.head++;
+    return value;
+  } else {
+    return null;
+  }
+};
+
+PseudoclassicalQueue.prototype.size = function() {
+  return this.tail - this.head;
+};
+
+export default PseudoclassicalQueue;
